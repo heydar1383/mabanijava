9ec1364c624b9717eae881dbf0c158d6bcdefa4b
heyadri <abolfazlheydari11338833@gmail.com>
2025-08-01 11:54:41 +0330

    car

--- javapishrafte/src/test/java/CarTest.java
+++ javapishrafte/src/test/java/CarTest.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class CarTest {
+
+     class Mashin extends CarTest {
+        boolean CarIsOn;
+        boolean DoorIsCloe;
+
+        void on() {
+            CarIsOn = true;
+        }
+
+        void off() {
+            CarIsOn = false;
+        }
+
+        void opendoor() {
+            DoorIsCloe = false;
+        }
+
+        void closedoor() {
+            DoorIsCloe = true;
+        }
+
+        void run() {
+            if (DoorIsCloe == true && CarIsOn == true) {
+                System.out.println("we can run");
+            }
+
+
+        }
+    }
 }
+
